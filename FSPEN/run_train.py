a3f7f000f2f55f9292206ec6df4dad4d62f40e0e
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2025-03-24 18:53:25 +0200

    update: implemented the logic behind first in_wav of the dataset

--- FSPEN/run_train.py
+++ FSPEN/run_train.py
@@ -11,60 +11,67 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class AudioDataset(Dataset):
-    def __init__(self, dataset, configs):
-        """
-        Dataset pentru încărcarea și preprocesarea semnalelor audio.
-        :param dataset: Setul de date (ex: 'train' sau 'test').
-        :param configs: Configurațiile (inclusiv parametrii pentru preprocesare).
-        """
-        self.dataset = dataset
+    def __init__(self, configs):
+        # Încărcarea seturilor de date
+        self.data_files = {
+            'train': [
+                '../../VoiceBank_DEMAND_16k/train/0000.parquet',
+                '../../VoiceBank_DEMAND_16k/train/0001.parquet',
+                '../../VoiceBank_DEMAND_16k/train/0002.parquet',
+                '../../VoiceBank_DEMAND_16k/train/0003.parquet',
+                '../../VoiceBank_DEMAND_16k/train/0004.parquet',
+            ],
+            'test': [
+                '../../VoiceBank_DEMAND_16k/test/0000.parquet'
+            ]}
+        self.dataset = load_dataset("parquet", data_files=self.data_files, streaming=True)
         self.configs = configs
 
     def __len__(self):
         return len(self.dataset)
 
-    def __getitem__(self, idx):
-        # Încărcăm datele audio din setul de date
-        sample = self.dataset[idx]
-
-        # Accesăm corect semnalele audio din dicționar
-        noisy_wav = torch.tensor(sample['noisy'], dtype=torch.float32)  # Conversie în tensor
-        clean_wav = torch.tensor(sample['clean'], dtype=torch.float32)  # Conversie în tensor
-
-        # Preprocesarea semnalului cu zgomot
-        noisy_wav = noisy_wav.squeeze(0)  # Îndepărtează dimensiunea redundantă (dacă există)
-        clean_wav = clean_wav.squeeze(0)  # Îndepărtează dimensiunea redundantă (dacă există)
-
-        # Transformarea semnalelor audio într-un spectru complex
-        noisy_complex_spectrum = torch.stft(noisy_wav, n_fft=self.configs.n_fft, hop_length=self.configs.hop_length,
-                                            window=torch.hamming_window(self.configs.n_fft), return_complex=True)
-        clean_complex_spectrum = torch.stft(clean_wav, n_fft=self.configs.n_fft, hop_length=self.configs.hop_length,
-                                            window=torch.hamming_window(self.configs.n_fft), return_complex=True)
-
-        # Calcularea amplitudinii
-        noisy_amplitude_spectrum = torch.abs(noisy_complex_spectrum)
-        clean_amplitude_spectrum = torch.abs(clean_complex_spectrum)
-
-        # Preprocesarea spectrului complex
-        noisy_complex_spectrum = torch.view_as_real(noisy_complex_spectrum)  # (B, F, T, 2)
-        clean_complex_spectrum = torch.view_as_real(clean_complex_spectrum)  # (B, F, T, 2)
-
-        noisy_complex_spectrum = torch.permute(noisy_complex_spectrum, dims=(0, 2, 3, 1))  # (B, T, F, 2)
-        clean_complex_spectrum = torch.permute(clean_complex_spectrum, dims=(0, 2, 3, 1))  # (B, T, F, 2)
-
-        # Asigurăm că dimensiunea este corectă
-        batch_size, frames, channels, frequency = noisy_complex_spectrum.shape
-        noisy_complex_spectrum = torch.reshape(noisy_complex_spectrum, shape=(batch_size, frames, channels, frequency))
-        clean_complex_spectrum = torch.reshape(clean_complex_spectrum, shape=(batch_size, frames, channels, frequency))
-
-        # Preprocesarea spectrului de amplitudine
-        noisy_amplitude_spectrum = torch.permute(noisy_amplitude_spectrum, dims=(0, 2, 1))  # (B, F, T)
-        clean_amplitude_spectrum = torch.permute(clean_amplitude_spectrum, dims=(0, 2, 1))  # (B, F, T)
-
-        noisy_amplitude_spectrum = torch.reshape(noisy_amplitude_spectrum, shape=(batch_size, frames, 1, frequency))
-        clean_amplitude_spectrum = torch.reshape(clean_amplitude_spectrum, shape=(batch_size, frames, 1, frequency))
-
-        return noisy_complex_spectrum, clean_complex_spectrum, noisy_amplitude_spectrum, clean_amplitude_spectrum
+    # def __getitem__(self, idx):
+    #     # Încărcăm datele audio din setul de date
+    #     sample = self.dataset[idx]
+    #
+    #     # Accesăm corect semnalele audio din dicționar
+    #     noisy_wav = torch.tensor(sample['noisy'], dtype=torch.float32)  # Conversie în tensor
+    #     clean_wav = torch.tensor(sample['clean'], dtype=torch.float32)  # Conversie în tensor
+    #
+    #     # Preprocesarea semnalului cu zgomot
+    #     noisy_wav = noisy_wav.squeeze(0)  # Îndepărtează dimensiunea redundantă (dacă există)
+    #     clean_wav = clean_wav.squeeze(0)  # Îndepărtează dimensiunea redundantă (dacă există)
+    #
+    #     # Transformarea semnalelor audio într-un spectru complex
+    #     noisy_complex_spectrum = torch.stft(noisy_wav, n_fft=self.configs.n_fft, hop_length=self.configs.hop_length,
+    #                                         window=torch.hamming_window(self.configs.n_fft), return_complex=True)
+    #     clean_complex_spectrum = torch.stft(clean_wav, n_fft=self.configs.n_fft, hop_length=self.configs.hop_length,
+    #                                         window=torch.hamming_window(self.configs.n_fft), return_complex=True)
+    #
+    #     # Calcularea amplitudinii
+    #     noisy_amplitude_spectrum = torch.abs(noisy_complex_spectrum)
+    #     clean_amplitude_spectrum = torch.abs(clean_complex_spectrum)
+    #
+    #     # Preprocesarea spectrului complex
+    #     noisy_complex_spectrum = torch.view_as_real(noisy_complex_spectrum)  # (B, F, T, 2)
+    #     clean_complex_spectrum = torch.view_as_real(clean_complex_spectrum)  # (B, F, T, 2)
+    #
+    #     noisy_complex_spectrum = torch.permute(noisy_complex_spectrum, dims=(0, 2, 3, 1))  # (B, T, F, 2)
+    #     clean_complex_spectrum = torch.permute(clean_complex_spectrum, dims=(0, 2, 3, 1))  # (B, T, F, 2)
+    #
+    #     # Asigurăm că dimensiunea este corectă
+    #     batch_size, frames, channels, frequency = noisy_complex_spectrum.shape
+    #     noisy_complex_spectrum = torch.reshape(noisy_complex_spectrum, shape=(batch_size, frames, channels, frequency))
+    #     clean_complex_spectrum = torch.reshape(clean_complex_spectrum, shape=(batch_size, frames, channels, frequency))
+    #
+    #     # Preprocesarea spectrului de amplitudine
+    #     noisy_amplitude_spectrum = torch.permute(noisy_amplitude_spectrum, dims=(0, 2, 1))  # (B, F, T)
+    #     clean_amplitude_spectrum = torch.permute(clean_amplitude_spectrum, dims=(0, 2, 1))  # (B, F, T)
+    #
+    #     noisy_amplitude_spectrum = torch.reshape(noisy_amplitude_spectrum, shape=(batch_size, frames, 1, frequency))
+    #     clean_amplitude_spectrum = torch.reshape(clean_amplitude_spectrum, shape=(batch_size, frames, 1, frequency))
+    #
+    #     return noisy_complex_spectrum, clean_complex_spectrum, noisy_amplitude_spectrum, clean_amplitude_spectrum
 
     def batches_generator(self, data):
         """YIELD (continuous "return") the current batches of self.configs.batch_size elements each"""
@@ -82,61 +89,53 @@ class AudioDataset(Dataset):
         if buffer:
             yield buffer
 
+    def convert_to_stft(self, in_wav):
+        # Generarea unui input pentru testare
+        # in_wav = torch.tensor(train_set["clean"][1])
+        in_wav = torch.randn(1, configs.train_points)
+        complex_spectrum = torch.stft(in_wav, n_fft=configs.n_fft, hop_length=configs.hop_length,
+                                      window=torch.hamming_window(configs.n_fft), return_complex=True)  # (B, F, T)
+        amplitude_spectrum = torch.abs(complex_spectrum)
 
-def create_dataloader(split, configs, batch_size=32):
-    """
-    Funcția pentru a crea DataLoader-ul pentru antrenare sau testare.
-    :param split: 'train' sau 'test'.
-    :param configs: Configurațiile de antrenament.
-    :param batch_size: Dimensiunea batch-ului.
-    :return: DataLoader
-    """
-    # Încărcarea setului de date din HuggingFace
-    dataset = load_dataset("JacobLinCool/VoiceBank-DEMAND-16k", split=split)
-
-    # Crearea dataset-ului personalizat
-    audio_dataset = AudioDataset(dataset, configs)
-
-    # Crearea DataLoader-ului
-    dataloader = DataLoader(audio_dataset, batch_size=batch_size, shuffle=True)
-
-    return dataloader
+        batch = 1
+        # Preprocesarea spectrului
+        complex_spectrum = torch.view_as_real(complex_spectrum)  # (B, F, T, 2)
+        complex_spectrum = torch.permute(complex_spectrum, dims=(0, 2, 3, 1))
+        _, frames, channels, frequency = complex_spectrum.shape
+        complex_spectrum = torch.reshape(complex_spectrum, shape=(batch, frames, channels, frequency))
+        amplitude_spectrum = torch.permute(amplitude_spectrum, dims=(0, 2, 1))
+        amplitude_spectrum = torch.reshape(amplitude_spectrum, shape=(batch, frames, 1, frequency))
 
 
 if __name__ == "__main__":
-    # Încărcarea seturilor de date
-    # train_set = load_dataset("JacobLinCool/VoiceBank-DEMAND-16k", split='train', streaming=True, num_proc=5)
-    # test_set = load_dataset("JacobLinCool/VoiceBank-DEMAND-16k", split='test', streaming=True, num_proc=5)
-    # print(train_set, test_set)
-    data_files = {
-        'train': [
-            '../../VoiceBank_DEMAND_16k/train/0000.parquet',
-            '../../VoiceBank_DEMAND_16k/train/0001.parquet',
-            '../../VoiceBank_DEMAND_16k/train/0002.parquet',
-            '../../VoiceBank_DEMAND_16k/train/0003.parquet',
-            '../../VoiceBank_DEMAND_16k/train/0004.parquet',
-        ],
-        'test': [
-            '../../VoiceBank_DEMAND_16k/test/0000.parquet'
-        ]}
-
-    dataset = load_dataset("parquet", data_files=data_files, streaming=True)
-    print(dataset)
-
     # Salvarea configurației de antrenare
     configs = TrainConfig()
     with open("config.json", mode="w", encoding="utf-8") as file:
         json.dump(configs.__dict__, file, indent=4)
     model = FullSubPathExtension(configs)
 
-    ds = AudioDataset(dataset, configs)
-    print(f"Length of ds: {ds.__len__()}\n")
-    test_batches = ds.batches_generator(ds.dataset['test'])
+    ds = AudioDataset(configs)
+    # print(ds.dataset)
     train_batches = ds.batches_generator(ds.dataset['train'])
+    test_batches = ds.batches_generator(ds.dataset['test'])
+
+    first_train_batch = next(train_batches)
+    first_sample = first_train_batch[0]
+    # print(first_sample['clean'])
+
+    # for batch in train_batches:
+    #     for sample in batch:
+    #         ds.convert_to_stft(sample)
+    #         print(sample)
+    #         break
 
     # Generarea unui input pentru testare
-    # in_wav = torch.tensor(train_set["clean"][1])
-    in_wav = torch.randn(1, configs.train_points)
+    # in_wav = torch.randn(1, configs.train_points)
+
+    # Introducerea unui input valid din VoiceBank_DEMAND_16k
+    in_wav = torch.tensor(first_sample['clean']['array'], dtype=torch.float32)
+    in_wav = in_wav.unsqueeze(0)
+    # print(f"Dimension in_wav: {in_wav.shape}")
     complex_spectrum = torch.stft(in_wav, n_fft=configs.n_fft, hop_length=configs.hop_length,
                                   window=torch.hamming_window(configs.n_fft), return_complex=True)  # (B, F, T)
     amplitude_spectrum = torch.abs(complex_spectrum)
